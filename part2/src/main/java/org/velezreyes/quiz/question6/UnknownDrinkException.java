9800f6b6bbc7e68803959e8871c1ab3db8e010a3
Julián Orozco V <jorozcova@unal.edu.co>
2023-11-02 11:12:35 -0500

    Revert "Adelanto parcial quiz1"
    This reverts commit ffb753fbc3cf8515973264fa91f795bf931ac9d5.

--- part2/src/main/java/org/velezreyes/quiz/question6/UnknownDrinkException.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/UnknownDrinkException.java
@@ -1,10 +1,8 @@
 package org.velezreyes.quiz.question6;
 
 public class UnknownDrinkException extends Exception {
-	
-	private static final long serialVersionUID = 1L;
 
-public UnknownDrinkException() {
+  public UnknownDrinkException() {
     super("Unknown drink!");
   }
 }
